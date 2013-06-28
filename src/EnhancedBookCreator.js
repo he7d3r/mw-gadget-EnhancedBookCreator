35c3dd3c553e30c11d72e009374cc3c40c381b24
Helder <he7d3r@gmail.com>
2013-06-28 15:25:51 +0000

    Add a closure and validate with JSHint

--- src/EnhancedBookCreator.js
+++ src/EnhancedBookCreator.js
@@ -1,30 +1,38 @@
 /**
  * @see: [[Tópico:Wikilivros:Diálogos comunitários/Aprimorando o Criador de livros]]
+ * @author: [[User:Helder.wiki]]
  * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/EnhancedBookCreator.js]] ([[File:User:Helder.wiki/Tools/EnhancedBookCreator.js]])
  */
+/*jshint browser: true, camelcase: true, curly: true, eqeqeq: true, immed: true, latedef: true, newcap: true, noarg: true, noempty: true, nonew: true, quotmark: true, undef: true, unused: true, strict: true, trailing: true, maxlen: 120, evil: true, onevar: true */
+/*global jQuery, mediaWiki */
+( function ( mw, $ ) {
+'use strict';
+
 document.getElementById( 'book-creator-normal' ).style.display = 'none';
-var aqui = document.getElementById( 'book-creator-melhorado' );
+var i, texto, botao, r,
+    aqui = document.getElementById( 'book-creator-melhorado' ),
+	op = [
+		document.getElementById( 'book-creator-op1' ),
+		document.getElementById( 'book-creator-op2' )
+	];
 aqui.style.display = 'block';
 
-var op = [ document.getElementById( 'book-creator-op1' ),
-          document.getElementById( 'book-creator-op2' ) ];
-
-for ( var i=0; i < op.length; i++ ){
+for ( i=0; i < op.length; i++ ){
 	op[i].style.display = 'none';
-	var botao = document.createElement( 'input' );
+	botao = document.createElement( 'input' );
 	botao.type = 'radio';
 	botao.name = 'criar';
 	botao.id = 'item_' + i;
 	botao.value = i;
 
-	if(i == 0) {
+	if(i === 0) {
 		botao.defaultChecked = true;
 		botao.checked = true;
 	}
 
-	var texto = document.createTextNode(op[i].innerHTML);
+	texto = document.createTextNode(op[i].innerHTML);
 
-	var r = document.createElement( 'label' );
+	r = document.createElement( 'label' );
 	r.htmlFor = botao.id;
 	r.appendChild(botao);
 	r.appendChild(texto);
@@ -33,19 +41,17 @@ for ( var i=0; i < op.length; i++ ){
 	aqui.appendChild(document.createElement( 'br' ));
 }
 
-
-
-
-
-if ( 'Especial:Livro' === mw.config.get( 'wgPageName' ) ) {
+if ( mw.config.get( 'wgCanonicalSpecialPageName' ) === 'Book' ) {
 	$(function () {
-		var savebox = document.getElementById( 'coll-savebox' );
-		var rightbox = savebox.parentNode;
-		var orderbox = rightbox.getElementsByTagName( 'div' )[0];
-		var downloadbox = rightbox.getElementsByTagName( 'div' )[3];
-		//rightbox.style.display = 'none';
+		var savebox = document.getElementById( 'coll-savebox' ),
+			rightbox = savebox.parentNode,
+			orderbox = rightbox.getElementsByTagName( 'div' )[0],
+			downloadbox = rightbox.getElementsByTagName( 'div' )[3];
+		// rightbox.style.display = 'none';
 		orderbox.style.display = 'none';
 		downloadbox.style.display = 'none';
-		//savebox.style.display = 'none';
+		// savebox.style.display = 'none';
 	});
 }
+
+}( mediaWiki, jQuery ) );
