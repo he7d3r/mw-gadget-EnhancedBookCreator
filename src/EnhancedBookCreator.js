b77e253551459338c991af58d616a6f6a746b002
Helder <he7d3r@gmail.com>
2014-09-27 20:48:58 -0300

    Apply MediaWiki Coding Conventions
    Validated with jscs 1.6.2, using preset "wikimedia"

--- src/EnhancedBookCreator.js
+++ src/EnhancedBookCreator.js
@@ -4,52 +4,52 @@
  * @license: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
  */
 ( function ( mw, $ ) {
-'use strict';
+	'use strict';
 
-document.getElementById( 'book-creator-normal' ).style.display = 'none';
-var i, texto, botao, r,
-    aqui = document.getElementById( 'book-creator-melhorado' ),
-	op = [
-		document.getElementById( 'book-creator-op1' ),
-		document.getElementById( 'book-creator-op2' )
-	];
-aqui.style.display = 'block';
+	document.getElementById( 'book-creator-normal' ).style.display = 'none';
+	var i, texto, botao, r,
+	aqui = document.getElementById( 'book-creator-melhorado' ),
+		op = [
+			document.getElementById( 'book-creator-op1' ),
+			document.getElementById( 'book-creator-op2' )
+		];
+	aqui.style.display = 'block';
 
-for ( i=0; i < op.length; i++ ){
-	op[i].style.display = 'none';
-	botao = document.createElement( 'input' );
-	botao.type = 'radio';
-	botao.name = 'criar';
-	botao.id = 'item_' + i;
-	botao.value = i;
+	for ( i = 0; i < op.length; i++ ) {
+		op[i].style.display = 'none';
+		botao = document.createElement( 'input' );
+		botao.type = 'radio';
+		botao.name = 'criar';
+		botao.id = 'item_' + i;
+		botao.value = i;
 
-	if(i === 0) {
-		botao.defaultChecked = true;
-		botao.checked = true;
+		if (i === 0) {
+			botao.defaultChecked = true;
+			botao.checked = true;
+		}
+
+		texto = document.createTextNode(op[i].innerHTML);
+
+		r = document.createElement( 'label' );
+		r.htmlFor = botao.id;
+		r.appendChild(botao);
+		r.appendChild(texto);
+
+		aqui.appendChild(r);
+		aqui.appendChild(document.createElement( 'br' ));
 	}
 
-	texto = document.createTextNode(op[i].innerHTML);
-
-	r = document.createElement( 'label' );
-	r.htmlFor = botao.id;
-	r.appendChild(botao);
-	r.appendChild(texto);
-
-	aqui.appendChild(r);
-	aqui.appendChild(document.createElement( 'br' ));
-}
-
-if ( mw.config.get( 'wgCanonicalSpecialPageName' ) === 'Book' ) {
-	$(function () {
-		var savebox = document.getElementById( 'coll-savebox' ),
-			rightbox = savebox.parentNode,
-			orderbox = rightbox.getElementsByTagName( 'div' )[0],
-			downloadbox = rightbox.getElementsByTagName( 'div' )[3];
-		// rightbox.style.display = 'none';
-		orderbox.style.display = 'none';
-		downloadbox.style.display = 'none';
-		// savebox.style.display = 'none';
-	});
-}
+	if ( mw.config.get( 'wgCanonicalSpecialPageName' ) === 'Book' ) {
+		$(function () {
+			var savebox = document.getElementById( 'coll-savebox' ),
+				rightbox = savebox.parentNode,
+				orderbox = rightbox.getElementsByTagName( 'div' )[0],
+				downloadbox = rightbox.getElementsByTagName( 'div' )[3];
+			// rightbox.style.display = 'none';
+			orderbox.style.display = 'none';
+			downloadbox.style.display = 'none';
+			// savebox.style.display = 'none';
+		});
+	}
 
 }( mediaWiki, jQuery ) );
